d140787d50937222b76bde4aebbdfe9ff8de9828
Jay McCure <jmccure@gmail.com>
2014-04-24 17:50:37 +1000

    aussie 5

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,6 +1,5 @@
 Before do |scenario|
   puts "Starting scenario: #{scenario.name}"
-  BalanceStore.new.balance = 0
   TransactionQueue.clear
 end
 
---
+++ lib/account.rb
@@ -0,0 +1,33 @@
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(:adapter => 'sqlite3',
+                                        :database => 'db/bank.db')
+
+ActiveRecord::Migrator.migrate("db/migrate/")
+class Account < ActiveRecord::Base
+  validates_uniqueness_of :number
+=begin
+  def initialize
+    @queue = TransactionQueue.new
+    @balance_store = BalanceStore.new
+  end
+=end
+
+  def queue
+    @queue ||=TransactionQueue.new
+  end
+
+  def credit(amount)
+    @queue.write("+#{amount},#{number}")
+  end
+
+=begin
+  def balance
+    @balance_store.balance
+  end
+=end
+
+  def debit(amount)
+    @queue.write("-#{amount},#{number}")
+  end
+end
